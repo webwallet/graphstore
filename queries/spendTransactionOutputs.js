a9e7175e5579b3b3971707a20c2d0096a93e83ac
jorgezaccaro <jorgezaccaro@gmail.com>
2017-03-28 22:12:31 +0800

    feat(queries): restructured response to include IOUs

--- queries/spendTransactionOutputs.js
+++ queries/spendTransactionOutputs.js
@@ -5,31 +5,34 @@ const query = `
   with spender
   unwind {inputs} as input
   merge (iou:IOU {id: input})-[:Funds]->(spender)
-  with spender, range(0, size({outputs})) as outputsRange, collect(iou) as ious
+  with spender, range(0, size({outputs})) as outputsRange, collect(iou.id) as ious
   unwind outputsRange as i
-  with spender, toString(i) as outputPosition, {outputs}[i] as output
+  with spender, toString(i) as outputPosition, {outputs}[i] as output, ious
   match (address:Address)-[index:Outputs]->(outputsIndex)
   where address.id=output.address and index.id=output.currency
   merge (spender)<-[:Points {id: outputPosition}]-(outputsIndex)
   merge (spender)<-[:Unspent {id: outputPosition}]-(outputsIndex)
-  with address, spender, outputsIndex, output
+  with address, spender, outputsIndex, output, ious
   match (outputsIndex)-[pointer:Unspent]->(spendee:Transaction)
   where (spendee.id + '::' + pointer.id) in output.previous
   and not (spendee)<-[:Spends {id: pointer.id}]-()
   create (spender)-[:Spends {id: pointer.id}]->(spendee)
   delete pointer
-  with collect(output) as outputs, output.previous as previous,
-  address.id as address, output.currency as currency
-  return address, currency, previous
+  with address, output, collect(output) as outs, ious
+  return ious as inputs,
+  collect({
+    address: address.id,
+    currency: output.currency,
+    previous: output.previous
+  }) as outputs
 `
 
 module.exports = {
   string: query.replace(/\n/g, '').trim(),
   parser(result) {
     return {
-      address: result.get('address'),
-      currency: result.get('currency'),
-      previous: result.get('previous')
+      inputs: result.get('inputs'),
+      outputs: result.get('outputs')
     }
   }
 }
